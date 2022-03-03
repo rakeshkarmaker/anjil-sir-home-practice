46b49979a37d3daf7dc6e5fc401a62f78cde0dd9
Rakesh Karmaker <rakeshkarmaker65@gmail.com>
2022-03-03 23:59:47 +0600

    Update 13.cpp

--- 13.cpp
+++ 13.cpp
@@ -10,18 +10,6 @@ Input the value for nth term: 5
 1+2+3+4+5 = 15
 The sum of the above series is: 35
 */
-/*
-13. Write a program in C++ to calculate the series (1) + (1+2) + (1+2+3) + (1+2+3+4) + ... +
-(1+2+3+4+...+n).
-Sample Output:
-Input the value for nth term: 5
-1 = 1
-1+2 = 3
-1+2+3 = 6
-1+2+3+4 = 10
-1+2+3+4+5 = 15
-The sum of the above series is: 35
-*/
 #include <iostream>
 using namespace std;
 #include <math.h>
