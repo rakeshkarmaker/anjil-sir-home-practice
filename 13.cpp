1f24e644fc3cd0a3def984d3d3f228f11f5fc015
Rakesh Karmaker <rakeshkarmaker65@gmail.com>
2022-03-04 00:36:10 +0600

    Update 13.cpp

--- 13.cpp
+++ 13.cpp
@@ -10,6 +10,27 @@ Input the value for nth term: 5
 1+2+3+4+5 = 15
 The sum of the above series is: 35
 */
+
+#include <iostream>
+using namespace std;
+#include <math.h>
+int main()
+{
+    int i,j, a,result=0,answer=0,x;
+    cout<<"type series steps : "<<endl;
+    cin>>a;
+    for(i = 1; i <= a; i++){
+        result = 0;
+        for(j = 1; j <= i; j++){
+            result = result+j;
+        }
+        answer = answer + result;
+        // cout << answer << endl;
+    }
+    cout<<"The result is: "<<answer << endl;
+    return 0;
+}
+/*
 #include <iostream>
 using namespace std;
 #include <math.h>
@@ -18,7 +39,36 @@ int main()
     int i,j, a,result=0,answer=0;
     cout<<"type series steps : "<<endl;
     cin>>a;
-    for(i=1;i<=a;i++){
+    for(i=1;i<=a;i++){  // (res+i)1=1;
+                        // 1 +(1+2)=4
+                        // 4 +(1+2+3)=10
+                        // 10 +(1+2+3+4)=20                                                
+        for( j=i;j<=i;j++){
+            result = result+j;
+        }
+    answer = answer+result;
+    }
+
+    cout<<"The result is: "<<answer;
+
+    return 0;
+}
+
+*/
+
+/*
+#include <iostream>
+using namespace std;
+#include <math.h>
+int main()
+{
+    int i,j, a,result=0,answer=0;
+    cout<<"type series steps : "<<endl;
+    cin>>a;
+    for(i=1;i<=a;i++){  // (res+i)1=1;
+                        // 1 +(1+2)=4
+                        // 4 +(1+2+3)=10
+                        // 10 +(1+2+3+4)=20 
         for( j=1;j<i;j++){
             result = result+j;
         }
@@ -29,4 +79,6 @@ int main()
 
     return 0;
 }
+*/
+
 
